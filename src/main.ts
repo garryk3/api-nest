322255ed3e634593b88e71abb8f6e7a0c2274057
garryk3 <egoruum@yandex.ru>
2019-05-12 17:44:37 +0300

    add port config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Swagger from './swagger';
+import config from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   new Swagger(app).init();
 
-  await app.listen(3000);
+  await app.listen(config.port);
 }
+
 bootstrap();
