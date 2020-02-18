ae9a16edcc026e862c7e1e797ae6a19234d4ac36
Igor <egoruum@yandex.ru>
2020-02-18 23:21:37 +0300

    up

--- src/helper.ts
+++ src/helper.ts
@@ -18,29 +18,62 @@ export async function standartRequest(transport: HttpService, params: DomoticzPa
 }
 
 export const idxNumbers: DeviceIdx =  {
-    waterheater: 16, // водонагреватель
+    waterheater: 17,
+    gateway: 9,
     light: {
-        room: 20,
-        hall: 21,
-        kitchen: 45
+        room: 12,
+        hall: 5,
+        kitchen: 1,
+        strip: 58
     },
     plugs: {
-        bathroomBoiler: 16,
-        kitchenDevicePanel: 7,
-        kitchenCommon: 8,
-        kitchenDeviceOven: 9,
-        kitchenDishwasher: 10,
-        kitchenMicrowave: 11,
-        kitchenWall: 12,
-        kitchenTV: 13,
-        roomTV: 25,
-        bathroomTowelDryer: 14,
-        bathroomKitchenWasher: 15,
-        roomCommon1: 17,
-        roomCommon2: 19,
-        roomPC: 18,
-        outerPlug1: 48,
-        outerPlug2: 50 
+        kitchenTV: 9,
+        kitchenKettle: 20,
+        kitchenDeviceOven: 4,
+        kitchenDishwasher: 13, 
+        kitchenMicrowave: 18,
+        kitchenYandex: 6,
+        bathWasher: 2,
+        bathDryer: 15,
+        bathBoiler: 17,
+        roomPC: 16,
+        roomCharger: 7, 
+        roomEmpty: 10,
+        roomTable: 8,
+        kitchenEmpty: 19, 
+        // externalDiff: 9999,
+        externalTV: 11
+    },
+    buttons: {
+        room: 42,
+        hall: 47,
+        bath: 53
+    },
+    switchers: {
+        room: 12,
+        hall: 5,
+        bath_1: 14,
+        bath_2: 44,
+        wirelessKitchen: 59 
+    },
+    sensors: {
+        motion: {
+            kitchen_1: 3,
+            kitchen_2: 52,
+            bath: 51,
+            hall: 50,
+            room: 55
+        },
+        smoke: {
+            room: 45,
+            kitchen: 46
+        },
+        water: {
+            room: 49
+        },
+        open: {
+            door: 48
+        }
     }
 };
 
@@ -53,7 +86,7 @@ export async function requestDeviceStatus(transport: HttpService, idx: number): 
     try {
         const response = await transport.get('/json.htm', { params }).toPromise();
         const data = response.data;
-        const { Data, Name, Status, Timers, LastUpdate } = data.result[0];
+        const { Data, Name, Status, Timers, LastUpdate, idx } = data.result[0];
 
         return {
             error: null,
@@ -62,7 +95,8 @@ export async function requestDeviceStatus(transport: HttpService, idx: number): 
                 name: Name,
                 status: Status,
                 timers: Timers,
-                lastupdate: LastUpdate
+                lastupdate: LastUpdate,
+                idx
             }
         };
     } catch (error) {
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -11,28 +11,61 @@ export interface DomoticzParams {
 
 export interface DeviceIdx {
     waterheater: number; // водонагреватель
+    gateway: number; // шлюз
     light: {
         room: number;
         hall: number;
         kitchen: number;
+        strip: number;
     };
     plugs: {
-        bathroomBoiler: number;
-        kitchenDevicePanel: number;
-        kitchenCommon: number;
+        kitchenTV: number;
+        kitchenKettle: number;
         kitchenDeviceOven: number;
         kitchenDishwasher: number;
         kitchenMicrowave: number;
-        kitchenWall: number;
-        kitchenTV: number;
-        roomTV: number;
-        bathroomTowelDryer: number;
-        bathroomKitchenWasher: number;
-        roomCommon1: number;
-        roomCommon2: number;
+        kitchenYandex: number;
+        bathWasher: number;
+        bathDryer: number;
+        bathBoiler: number;
         roomPC: number;
-        outerPlug1: number;
-        outerPlug2: number;
+        roomCharger: number;
+        roomEmpty: number;
+        roomTable: number;
+        kitchenEmpty: number;
+        externalDiff?: number;
+        externalTV: number;
+    };
+    buttons: {
+        room: number;
+        hall: number;
+        bath: number;
+    };
+    switchers: {
+        room: number;
+        hall: number;
+        bath_1: number;
+        bath_2: number;
+        wirelessKitchen: number;
+    };
+    sensors: {
+        motion: {
+            kitchen_1: number;
+            kitchen_2: number;
+            bath: number;
+            hall: number;
+            room: number;
+        };
+        smoke: {
+            room: number;
+            kitchen: number;
+        };
+        water: {
+            room: number;
+        };
+        open: {
+            door: number;
+        }
     }
 }
 
